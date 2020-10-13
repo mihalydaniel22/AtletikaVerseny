07edcedbec32941cb3b9577bad9f01102760f296
mihalydaniel22 <rikkiardo3@gmail.com>
2020-10-13 08:20:44 +0200

    Add project files.

---
+++ AtletikaiVerseny/Atleta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtletikaiVerseny
+{
+    class Atleta
+    {
+        public string Rajtszam { get; private set; }
+        public string VezNev { get; private set; }
+        public string KerNev { get; private set; }
+        public string Egyesület { get; private set; }
+        public int Ugras { get; private set; }
+
+        public Atleta(string a)
+        {           
+            Rajtszam = Convert.ToInt32(a[0]);
+            VezNev = Convert.ToInt32(a[1]);
+            KerNev = Convert.ToInt32(a[2])
+            Egyesület = Convert.ToInt32(a[3]);
+            Ugras = a[4];
+        }
+    }
+}
---
+++ AtletikaiVerseny/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AtletikaiVerseny
+{
+    class Program
+    {
+        static List<Atleta> versenyzok = new List<Atleta>();
+
+        public void Beolvasas()
+        {
+            StreamReader file = new StreamReader("tavol.csv");
+            while (!file.EndOfStream)
+            {
+                versenyzok.Add(Console.ReadLine(file));
+            }
+            file.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            Beolvasas();
+            Console.ReadKey();
+        }
+    }
+}
